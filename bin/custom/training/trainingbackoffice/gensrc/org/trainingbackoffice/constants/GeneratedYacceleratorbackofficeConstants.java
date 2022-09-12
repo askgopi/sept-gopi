9ce55c4e0cebda91405601d748ca4cbe6183f812
askgopi <askgopi@outlook.com>
2022-09-12 06:06:58 -0500

    First Hybris sept

---
+++ bin/custom/training/trainingbackoffice/gensrc/org/trainingbackoffice/constants/GeneratedYacceleratorbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at May 29, 2022 9:32:12 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package org.trainingbackoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedYacceleratorbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "trainingbackoffice";
+	
+	protected GeneratedYacceleratorbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ bin/custom/training/traininginitialdata/gensrc/com/hybris/training/initialdata/constants/GeneratedTrainingInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at May 29, 2022 9:32:12 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.hybris.training.initialdata.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedTrainingInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "traininginitialdata";
+	
+	protected GeneratedTrainingInitialDataConstants()
+	{
+		// private constructor
+	}
+	
+	
+}

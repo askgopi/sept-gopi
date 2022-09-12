654c30bab24315e8f3c3320f8975bc6546ff3532
askgopi <askgopi@outlook.com>
2022-09-12 04:53:54 -0500

    auguest branch test modifief

--- LearningGIT3/src/main/java/test.java
+++ LearningGIT3/src/main/java/test.java
@@ -2,6 +2,7 @@ public test
         {
 
             public static void main {
-                System.out.ptintln ("hello Java");
+                System.out.ptintln ("hello Java Auguest Branch");
+                System.out.println("auguest changes ");
         }
         }
